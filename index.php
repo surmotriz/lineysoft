b106319f5a895f199abaeb7d599dfa34449d4701
sistemas <sitemas@surmotriz.com>
2017-11-01 19:23:45 -0500

    base

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include "layout/header.php" ?>
+<main class="bd-masthead" id="content" role="main">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-6 mx-auto col-md-6 order-md-2">
+                <img class="img-fluid mb-3 mb-md-0" src="assets/img/dashboard-img-3.png" alt="" width="800" height="860">
+            </div>
+            <div class="col-md-6 order-md-1 text-center text-md-left pr-md-5">
+                <h1 class="mb-3 bd-text-purple-bright">Factu Electrónica</h1>
+                <p class="lead">
+                    Build responsive, mobile-first projects on the web with the world's most popular front-end component
+                    library.
+                </p>
+                <p class="lead mb-4">
+                    Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. Quickly prototype your
+                    ideas or build your entire app with our Sass variables and mixins, responsive grid system, extensive
+                    prebuilt components, and powerful plugins built on jQuery.
+                </p>
+                <div class="d-flex flex-column flex-md-row lead mb-3">
+                    <a href="#" class="btn btn-lg btn-bd-purple mb-3 mb-md-0 mr-md-3" >¿Qué es.. ?</a>
+                    <a href="#" class="btn btn-lg btn-outline-secondary" >Probar Demo</a>
+                </div>
+                <p class="text-muted mb-0">
+                    Actualmente la version demo es V0.1
+                </p>
+            </div>
+        </div>
+    </div>
+</main>
+<div class="masthead-followup row m-0 border border-white">
+    <div class="col-12 col-md-4 p-3 p-md-5 bg-light border border-white">
+        <!-- Icon by Bytesize https://github.com/danklammer/bytesize-icons -->
+        <svg xmlns="http://www.w3.org/2000/svg" focusable="false" width="32" height="32" fill="none"
+             stroke="currentcolor" stroke-width="2" class="text-primary mb-2" viewBox="0 0 32 32" stroke-linecap="round"
+             stroke-linejoin="round">
+            <title>Import icon</title>
+            <path d="M28 22v8H4v-8M16 4v20m-8-8l8 8 8-8"/>
+        </svg>
+        <h3>Para Desarrolladores</h3>
+        <p>Include Bootstrap's source Sass and JavaScript files via npm, Composer or Meteor. Package managed installs
+            don't include documentation, but do include our build system and readme.
+        </p>
+        <hr class="half-rule">
+        <a class="btn btn-outline-primary" href="#">Info Desarrolladores</a>
+    </div>
+    <div class="col-12 col-md-4 p-3 p-md-5 bg-light border border-white">
+        <!-- Icon by Bytesize https://github.com/danklammer/bytesize-icons -->
+        <svg xmlns="http://www.w3.org/2000/svg" focusable="false" width="32" height="32" fill="none"
+             stroke="currentcolor" stroke-width="2" class="text-primary mb-2" viewBox="0 0 32 32" stroke-linecap="round"
+             stroke-linejoin="round">
+            <title>Download icon</title>
+            <path d="M9 22c-9 1-8-10 0-9C6 2 23 2 22 10c10-3 10 13 1 12m-12 4l5 4 5-4m-5-10v14"/>
+        </svg>
+        <h3>Para Empresas</h3>
+        <p>When you only need to include Bootstrap's compiled CSS or JS, you can use the Bootstrap CDN.</p>
+        <h5>CSS only</h5>
+        <hr class="half-rule">
+        <a class="btn btn-outline-primary" href="#">Info Empresas</a>
+    </div>
+    <div class="col-12 col-md-4 p-3 p-md-5 bg-light border border-white">
+        <!-- Icon by Bytesize https://github.com/danklammer/bytesize-icons -->
+        <svg xmlns="http://www.w3.org/2000/svg" focusable="false" width="32" height="32" fill="none"
+             stroke="currentcolor" stroke-width="2" class="text-primary mb-2" viewBox="0 0 32 32" stroke-linecap="round"
+             stroke-linejoin="round">
+            <title>Lightning icon</title>
+            <path d="M18 13l8-11L8 13l6 6-8 11 18-11z"/>
+        </svg>
+        <h3>Para Personas</h3>
+        <p>
+            Take Bootstrap 4 to the next level with official premium themes—toolkits built on Bootstrap with new
+            components and plugins, docs, and build tools.
+        </p>
+        <hr class="half-rule">
+        <a href="#" class="btn btn-outline-primary">Info Personas</a>
+    </div>
+</div>
+
+<?php include "layout/footer.php" ?>
---
+++ layout/footer.php
@@ -0,0 +1,30 @@
+
+<footer class="bd-footer text-muted">
+    <div class="container-fluid p-3 p-md-5">
+        <ul class="bd-footer-links">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Examples</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+        <p>
+            Designed and built with all the love in the world by
+            <a href="#" rel="noopener">@mdo</a>
+            and <a href="#" rel="noopener">@fat</a>
+            . Maintained by the <a href="#">core team</a>
+            with the help of <a href="#">our contributors</a>.
+        </p>
+        <p>
+            Currently v4.0.0-beta.2. Code licensed
+            <a href="#" rel="license noopener">MIT</a>
+            , docs <a href="#" rel="license noopener">CC BY 3.0</a>.
+        </p>
+    </div>
+</footer>
+<script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="assets/js/ie-emulation-modes-warning.js"></script>
+</body>
+</html>
